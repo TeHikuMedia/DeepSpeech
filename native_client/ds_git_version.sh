d426204f530247733798e4791e380aada9c6c89c
Caleb Moses <calebjdmoses@gmail.com>
2019-02-17 19:03:56 +1300

    Remove git dir check - it fails if DeepSpeech is checked in as a submodule

--- native_client/ds_git_version.sh
+++ native_client/ds_git_version.sh
@@ -4,10 +4,10 @@ if [ `uname` = "Darwin" ]; then
    export PATH="/Users/build-user/TaskCluster/Workdir/tasks/tc-workdir/homebrew/opt/coreutils/libexec/gnubin:${PATH}"
 fi
 
-GIT_DIR="$(realpath "$(dirname "$(realpath "$0")")/../.git/")"
-if [ ! -d "${GIT_DIR}" ]; then
-   return 1
-fi;
+# GIT_DIR="$(realpath "$(dirname "$(realpath "$0")")/../.git/")"
+# if [ ! -d "${GIT_DIR}" ]; then
+#    return 1
+# fi;
 
 GIT_VERSION=$(git --git-dir="${GIT_DIR}" describe --long --tags)
 if [ $? -ne 0 ]; then
