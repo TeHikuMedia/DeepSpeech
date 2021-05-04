bd9628ffa3dfc7a973ce4258cddf65db257506aa
Kiarie <73854284+ki-arie@users.noreply.github.com>
2021-05-05 10:43:54 +1200

    Took out python specific wheel

--- native_client/python/make_test_python.sh
+++ native_client/python/make_test_python.sh
@@ -1,5 +1,5 @@
 make bindings-clean
 pip uninstall deepspeech -y
 make bindings
-pip install dist/deepspeech-0.9.1-cp37-cp37m-macosx_10_9_x86_64.whl
+pip install dist/*.whl
 python client.py --model optimized_model.tflite --scorer lm.scorer --audio 172.wav
